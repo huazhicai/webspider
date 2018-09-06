45600ee13dd99bb0cee3711213920fc2878e1972
zhihua cai <zhihua cai>
2018-09-06 18:15:54 +0800

    proxypool

---
+++ ProxyPools/importer.py
@@ -0,0 +1,5 @@
+from proxypool.importer import scan
+
+
+if __name__ == '__main__':
+    scan()
--- seven.py
+++ seven.py
@@ -1,18 +1,27 @@
-import aiohttp
 import asyncio
 
-
-async def fetch(session, url):
-    async with session.get(url) as response:
-        return await response.text()
+from aiohttp import web
 
 
-async def main():
-    async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'http://pyhon.org')
-        print(html)
+async def index(request):
+    await asyncio.sleep(3)
+    return web.Response(body=b'<h1>Index</h1>')
 
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+async def hello(request):
+    await asyncio.sleep(3)
+    text = '<h1>hello, %s?</h1>' % request.match_info['name']
+    return web.Response(body=text.encode('utf-8'))
+
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    app.router.add_route('GET', '/hello/{name}', hello)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 8000)
+    return srv
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
