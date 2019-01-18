63359260ff43f3c83b5b874ed2f339ec182f7211
huazhicai <936844218@qq.com>
2019-01-18 23:38:27 +0800

    tutorial

---
+++ tutorial/readme.md
@@ -0,0 +1,30 @@
+## scrapy tutorial
+
+- **创建Spider**
+    - scrapy startproject tutorial && cd tutorial
+    - scrapy genspider quotes quotes.toscrape.com
+    - name 每个项目的唯一名字，用来区分不同的的Spider
+    - allowed_domains 如果初始或后续的请求链接不是这个域名下的，则请求链接会被过滤掉
+    - start_urls 定义初始请求
+    - parse 解析返回的响应
+
+- **运行爬虫**
+    - scrapy crawl quotes
+    - scrapy crawl quotes -o quotes.json/quotes.jl (jsonline的缩写)
+    - scrapy crawl quotes -o quotes.csv/quotes.xml/quotes.pickle
+    - ftp://user:password@ftp.example.com/path/to/quotes.csv
+
+- **Item Pipeline 项目管道**
+    - 清理HTML数据
+    - 验证爬虫数据，检查爬虫字段
+    - 查重并丢弃重复内容
+    - 将爬取的结果保存到数据库
+    
+    - 定义一个类实现process_item()方法即可
+    - @classmethod  # 类方法在此方法内部使用类和类属性而不需要实例调用
+- ****
+- ****
+- ****
+- ****
+- ****
+- ****
---
+++ tutorial/tutorial/spiders/quotes.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from tutorial.items import QuoteItem
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        quotes = response.css('.quote')
+        for quote in quotes:
+            item = QuoteItem()
+            item['text'] = quote.css('.text::text').extract_first()
+            item['author'] = quote.css('.author::text').extract_first()
+            item['tags'] = quote.css('.tags .tag::text').extract()
+            yield item
+
+        next = response.css('.pager .next a::attr(href)').extract_first()
+        # urljoin() 方法将相对的url构造成绝对的url
+        url = response.urljoin(next)
+        yield scrapy.Request(url=url, callback=self.parse)
